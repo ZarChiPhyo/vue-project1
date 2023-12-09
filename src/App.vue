a20edc8f7a86cc2d0c9825839497bbb39a389ab1
ZarChiPhyo <zarchiphyoe11@gmail.com>
2023-12-09 15:15:07 +0630

    first commit added files

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <DataBindingComponentVue></DataBindingComponentVue>
+</template>
+
+<script>
+import DataBindingComponentVue from './components/DataBindingComponent.vue'
+
+
+export default {
+  name: 'App',
+  components: {
+    DataBindingComponentVue
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/DataBindingComponent.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+        <h3>Data Binding</h3>
+        <p>Name:{{ name }}</p>
+        <p>Mail:{{ mail }}</p>
+        <p>Marks:{{ marks[0] }},{{ marks[1] }},{{ marks[2] }}</p>
+        <p v-bind:class="levelClass">Levels:html:{{ levels.html }},css:{{levels.css}},js:{{ levels.js }},vue:{{ levels.vue }}</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DataBinding',
+        data(){
+            return{
+                name:"Thiri Aung",
+                mail:"thiri@gmail.com",
+                marks:[56,78,90],
+                levels:{
+                    html:5,
+                    css:4,
+                    js:4,
+                    vue:5
+                },
+                levelClass:'capital',
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    .capital{
+        text-transform: uppercase;
+        font-family: Georgia, 'Times New Roman', Times, serif;
+        font-size: 20px;
+    }
+</style>
